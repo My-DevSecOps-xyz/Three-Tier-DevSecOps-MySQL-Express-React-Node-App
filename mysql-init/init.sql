60e24ee1867cd277e9190f079f874d1103e3d7f1
Princewill Opah <98644246+princewill-opah@users.noreply.github.com>
2025-07-08 12:57:18 +0100

    Feature/improve UI db (#10)
    * Add new feature
    
    * Add new feature
    
    * Add new feature
    
    * Add new feature
    
    * Add new feature
    
    * Add new feature
    
    * Add new feature
    
    * Add new feature
    
    * Add new feature
    
    * Add new feature
    
    * Add new feature
    
    * Add new feature
    
    * Add new feature latest
    
    * Add new feature latest
    
    * Add new feature latest
    
    * Add new feature latest
    
    * good
    
    * Added new updates
    
    * updated
    
    * updated
    
    * updated
    
    * updated
    
    ---------
    
    Co-authored-by: princewillopah <princewillopah@gmail.com>

---
+++ mysql-init/init.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS crud_app;
+
+USE crud_app;
+
+CREATE TABLE IF NOT EXISTS users (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  role ENUM('admin', 'viewer') NOT NULL DEFAULT 'viewer',
+  is_active TINYINT(1) DEFAULT 1,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
+-- INSERT INTO users (name, email, password, role) 
+-- VALUES ('Admin User', 'popo@popo.com', '$2a$10$qz8klo31TOUGIJv78sC5OegkdkOq/SqHhSMzYNUKn2FTm7WHwPfF.', 'admin');
